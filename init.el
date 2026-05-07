f634d78dd1141678d5e0ec85e57b8e10143c9149
LeeSpringFly <772890143@qq.com>
2026-05-07 17:40:47 +0900

    Add files via upload

---
+++ init.el
@@ -0,0 +1,37 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+;; 设置默认编码为 UTF-8
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'zenburn t)
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+(require 'init-package)
+(require 'init-simple)
+(require 'init-ui)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(keyboard-coding-system 'utf-8)
+ '(package-selected-packages nil)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Courier New" :foundry "outline" :slant normal :weight normal :height 102 :width normal)))))
+
+
+
